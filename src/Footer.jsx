ebc3cde3064725b43534213c7bbecc9dd4abd883
Ubaid_Javid <ubaidjavaid430@gmail.com>
2025-11-03 22:44:09 +0530

    added footer

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+   <div className="text-6xl h-96 bg-blue-400 text-pink-500 flex items-center justify-center text-center">
+  Footer
+</div>
+
+  )
+}
+
+export default Footer
